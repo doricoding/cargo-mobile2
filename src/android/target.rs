83ed3fcfd9f40f09795abc2036500301d6ea7977
Dori Coding <111389929+doricoding@users.noreply.github.com>
2024-03-25 13:08:56 +0100

    Revert "fix(android): build rustc path to `.cargo` couldn't contain spaces (fix #6627)"

--- src/android/target.rs
+++ src/android/target.rs
@@ -213,10 +213,9 @@ impl<'a> Target<'a> {
             linker: Some(linker),
             rustflags: vec![
                 "-L".to_owned(),
-                format!("\"{}\"", dunce::simplified(&config.app().prefix_path(".cargo"))
+                dunce::simplified(&config.app().prefix_path(".cargo"))
                     .display()
-                    .to_string()
-                ),
+                    .to_string(),
                 "-Clink-arg=-landroid".to_owned(),
                 "-Clink-arg=-llog".to_owned(),
                 "-Clink-arg=-lOpenSLES".to_owned(),
